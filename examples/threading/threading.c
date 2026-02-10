01895cc94af4be5d6eadd6334f7659b2e3b5422f
Bryan Robinson <brobinson.eng@gmail.com>
2026-02-10 01:31:42 +0000

    Completed assignment 4.1; unit tests run successfully

--- examples/threading/threading.c
+++ examples/threading/threading.c
@@ -10,24 +10,40 @@
 
 void* threadfunc(void* thread_param)
 {
-
-    // TODO: wait, obtain mutex, wait, release mutex as described by thread_data structure
-    // hint: use a cast like the one below to obtain thread arguments from your parameter
-    //struct thread_data* thread_func_args = (struct thread_data *) thread_param;
+    struct thread_data *data = (struct thread_data *)(thread_param);
+    nanosleep(&data->wait_time_ns, NULL);
+    pthread_mutex_lock(data->pMutex);
+    nanosleep(&data->hold_time_ns, NULL);
+    data->thread_complete_success = true;
+    pthread_mutex_unlock(data->pMutex);
     return thread_param;
 }
 
 
 bool start_thread_obtaining_mutex(pthread_t *thread, pthread_mutex_t *mutex,int wait_to_obtain_ms, int wait_to_release_ms)
 {
-    /**
-     * TODO: allocate memory for thread_data, setup mutex and wait arguments, pass thread_data to created thread
-     * using threadfunc() as entry point.
-     *
-     * return true if successful.
-     *
-     * See implementation details in threading.h file comment block
-     */
-    return false;
+    struct thread_data *data = (struct thread_data *)malloc(sizeof(struct thread_data));
+    if (data == NULL) {
+        return false;
+    }
+
+    struct timespec wait_time_ns, hold_time_ns;
+    wait_time_ns.tv_sec = wait_to_obtain_ms / 1000;
+    wait_time_ns.tv_nsec = (wait_to_obtain_ms % 1000) * 1000000;
+    hold_time_ns.tv_sec = wait_to_release_ms / 1000;
+    hold_time_ns.tv_nsec = (wait_to_release_ms % 1000) * 1000000;
+    data->pMutex = mutex;
+    data->wait_time_ns = wait_time_ns;
+    data->hold_time_ns = hold_time_ns;
+    data->thread_complete_success = false;
+
+    int result = pthread_create(thread, NULL, threadfunc, data);
+    if (result != 0) {
+        free(data);
+	return false;
+    }
+    else {
+    	return true;
+    }
 }
 
--- examples/threading/threading.h
+++ examples/threading/threading.h
@@ -1,24 +1,12 @@
 #include <stdbool.h>
 #include <pthread.h>
+#include <unistd.h>
+#include <time.h>
 
-/**
- * This structure should be dynamically allocated and passed as
- * an argument to your thread using pthread_create.
- * It should be returned by your thread so it can be freed by
- * the joiner thread.
- */
 struct thread_data{
-    /*
-     * TODO: add other values your thread will need to manage
-     * into this structure, use this structure to communicate
-     * between the start_thread_obtaining_mutex function and
-     * your thread implementation.
-     */
-
-    /**
-     * Set to true if the thread completed with success, false
-     * if an error occurred.
-     */
+    pthread_mutex_t *pMutex;
+    struct timespec wait_time_ns;
+    struct timespec hold_time_ns;
     bool thread_complete_success;
 };
 
